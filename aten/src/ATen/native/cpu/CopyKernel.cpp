axanitzozra/pytorch

commit c075c8f71af96f72fcecfdd04320fbb901424e55

Simplify copy kernel with static_cast_with_inter_type (#29631)
Summary:
After https://github.com/pytorch/pytorch/pull/29612 get merged, `static_cast_with_inter_type` can now automatically convert complex types to its real values, therefore there is no need to do it inside copy kernel.

This should wait until https://github.com/pytorch/pytorch/pull/29612 get merged, otherwise it won't pass CI.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/29631

Differential Revision: D18485676

Pulled By: ezyang

fbshipit-source-id: 0bbfd551e3d3010f87eef0fce23a1f8a094b7d31

aten/src/ATen/native/cpu/CopyKernel.cpp
@@ -10,62 +10,6 @@ namespace at {
 namespace native {
 namespace {
 
-template <typename self_T>
-void copy_kernel_cast(TensorIterator& iter) {
-    if (isComplexType(iter.dtype(1))) {
-      AT_DISPATCH_COMPLEX_TYPES(iter.dtype(1), "copy_kernel_cast", [&] {
-        cpu_kernel(iter, [=](scalar_t a) -> self_T {
-            return c10::static_cast_with_inter_type<self_T>(std::real(a));
-          });
-        });
-    }
-    else {
-      AT_DISPATCH_ALL_TYPES_AND3(
-        ScalarType::Half,
-        ScalarType::Bool,
-        ScalarType::BFloat16,
-        iter.dtype(1),
-        "copy_kernel_cast",
-        [&] {
-          cpu_kernel(iter, [=](scalar_t a) -> self_T {
-            return c10::static_cast_with_inter_type<self_T>(a);
-          });
-        });
-    }
-}
-
-template <>
-void copy_kernel_cast<std::complex<float>>(TensorIterator& iter) {
-  // Specialization for inter-complex dtypes
-  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
-    ScalarType::Half,
-    ScalarType::Bool,
-    ScalarType::BFloat16,
-    iter.dtype(1),
-    "copy_kernel_cast",
-    [&] {
-      cpu_kernel(iter, [=](scalar_t a) -> std::complex<float> {
-        return c10::static_cast_with_inter_type<std::complex<float>>(a);
-      });
-    });
-}
-
-template <>
-void copy_kernel_cast<std::complex<double>>(TensorIterator& iter) {
-  // Specialization for inter-complex dtypes
-  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
-    ScalarType::Half,
-    ScalarType::Bool,
-    ScalarType::BFloat16,
-    iter.dtype(1),
-    "copy_kernel_cast",
-    [&] {
-      cpu_kernel(iter, [=](scalar_t a) -> std::complex<double> {
-        return c10::static_cast_with_inter_type<std::complex<double>>(a);
-      });
-    });
-}
-
 static void copy_kernel(TensorIterator& iter, bool non_blocking) {
   ScalarType dtype = iter.dtype(0);
   if (dtype == iter.dtype(1)) {
@@ -96,7 +40,10 @@ static void copy_kernel(TensorIterator& iter, bool non_blocking) {
     }
   } else {
     AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(ScalarType::Half, ScalarType::Bool, ScalarType::BFloat16, dtype, "copy_", [&] {
-      copy_kernel_cast<scalar_t>(iter);
+      using dest_t = scalar_t;
+      AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(ScalarType::Half, ScalarType::Bool, ScalarType::BFloat16, iter.dtype(1), "copy_", [&] {
+        cpu_kernel(iter, c10::static_cast_with_inter_type<dest_t, scalar_t>);
+      });
     });
   }
 }

commit 1dea29ef16b83fc95e4929fb27b595ae010ed316

Fix copy kernel speed regression introduced in #29631 (#31279)
Summary:
Fixes https://github.com/pytorch/pytorch/issues/31271

This fixes copy kernel speed regression introduced in https://github.com/pytorch/pytorch/issues/29631.

The previous implementation forces the compiler to instantiate `static_cast_with_inter_type` because it is passed as an argument of a function. This behavior makes it impossible for compilers to do optimizations like automatic vectorization, and, function call itself is expensive compared to a single casting instruction.

To check the change, run
```
readelf -Ws /home/xgao/anaconda3/lib/python3.7/site-packages/torch/lib/libtorch_cpu.so | grep static_cast_with_inter_type
```

On nightly build, we have output
```
168217: 0000000001852bf0     5 FUNC    LOCAL  DEFAULT    9 _ZN3c1027static_cast_with_inter_typeIsdE5applyEd
168816: 0000000001852d30    33 FUNC    LOCAL  DEFAULT    9 _ZN3c1027static_cast_with_inter_typeISt7complexIfEaE5applyEa
168843: 00000000018531f0     7 FUNC    LOCAL  DEFAULT    9 _ZN3c1027static_cast_with_inter_typeIblE5applyEl
168930: 0000000001852c20     3 FUNC    LOCAL  DEFAULT    9 _ZN3c1027static_cast_with_inter_typeIslE5applyEl
168935: 00000000018528d0   124 FUNC    LOCAL  DEFAULT    9 _ZN3c1027static_cast_with_inter_typeIfNS_4HalfEE5applyES1_
169023: 0000000001852f30    17 FUNC    LOCAL  DEFAULT    9 _ZN3c1027static_cast_with_inter_typeISt7complexIdEhE5applyEh
169713: 00000000018525c0     3 FUNC    LOCAL  DEFAULT    9 _ZN3c1027static_cast_with_inter_typeIahE5applyEh
170033: 0000000001852c10     3 FUNC    LOCAL  DEFAULT    9 _ZN3c1027static_cast_with_inter_typeIsiE5applyEi
170105: 0000000001852bd0     5 FUNC    LOCAL  DEFAULT    9 _ZN3c1027static_cast_with_inter_typeIshE5applyEh
170980: 0000000001852fc0    27 FUNC    LOCAL  DEFAULT    9 _ZN3c1027static_cast_with_inter_typeISt7complexIdES1_IfEE5applyES3_
171398: 0000000001852810    13 FUNC    LOCAL  DEFAULT    9 _ZN3c1027static_cast_with_inter_typeIdbE5applyEb

aten/src/ATen/native/cpu/CopyKernel.cpp
@@ -42,7 +42,22 @@ static void copy_kernel(TensorIterator& iter, bool non_blocking) {
     AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(ScalarType::Half, ScalarType::Bool, ScalarType::BFloat16, dtype, "copy_", [&] {
       using dest_t = scalar_t;
       AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(ScalarType::Half, ScalarType::Bool, ScalarType::BFloat16, iter.dtype(1), "copy_", [&] {
-        cpu_kernel(iter, c10::static_cast_with_inter_type<dest_t, scalar_t>::apply);
+        // Note (@zasdfgbnm):
+        //
+        // The code below can not be simplified as
+        //    cpu_kernel(iter, c10::static_cast_with_inter_type<dest_t, scalar_t>::apply);
+        //
+        // because this would force the compiler to instantiate the inline function and generate a function call in the loop
+        // instead of inlining it, making all the optimizations like vectorization impossible.
+        // You can verify this by looking the the symbols of `libtorch_cpu.so`:
+        //
+        //    readelf -Ws libtorch_cpu.so | grep static_cast_with_inter_type
+        //
+        // If done correctly, the above command should have no output.
+        //
+        // See: https://github.com/pytorch/pytorch/issues/31271
+        cpu_kernel(iter, [](scalar_t src) -> dest_t {
+          return c10::static_cast_with_inter_type<dest_t, scalar_t>::apply(src); });
       });
     });
   }