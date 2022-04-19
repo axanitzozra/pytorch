axanitzozra/pytorch

commit 6403ba76f20ebca0bf56963d11758d34ceca1a9d

Update circleci pipelines to support both master and main branches. (#73224)
Summary:
Fixes #71806

Pull Request resolved: https://github.com/pytorch/pytorch/pull/73224
Approved by: https://github.com/janeyx99, https://github.com/seemethere, https://github.com/malfet

Test Plan: contbuild & OSS CI, see https://hud.pytorch.org/commit/pytorch/pytorch/4646caede96d69fd8f7ffbf99e7c065b32bd6b01

Reviewed By: osalpekar

Differential Revision: D34928745

Pulled By: musebc

fbshipit-source-id: 30b0019aab976299338031b4d5b3cdfca9e445bb

.circleci/scripts/cpp_doc_push_script.sh
@@ -20,7 +20,7 @@ echo "cpp_doc_push_script.sh: Invoked with $*"
 #       but since DOCS_INSTALL_PATH can be derived from DOCS_VERSION it's probably better to
 #       try and gather it first, just so we don't potentially break people who rely on this script
 # Argument 2: What version of the Python API docs we are building.
-version="${2:-${DOCS_VERSION:-master}}"
+version="${2:-${DOCS_VERSION:-main}}"
 if [ -z "$version" ]; then
 echo "error: cpp_doc_push_script.sh: version (arg2) not specified"
   exit 1
@@ -34,9 +34,9 @@ echo "error: cpp_doc_push_script.sh: install_path (arg1) not specified"
   exit 1
 fi
 
-is_master_doc=false
-if [ "$version" == "master" ]; then
-  is_master_doc=true
+is_main_doc=false
+if [ "$version" == "main" ]; then
+  is_main_doc=true
 fi
 
 echo "install_path: $install_path  version: $version"

.circleci/scripts/python_doc_push_script.sh
@@ -23,7 +23,7 @@ set -ex
 #       but since DOCS_INSTALL_PATH can be derived from DOCS_VERSION it's probably better to
 #       try and gather it first, just so we don't potentially break people who rely on this script
 # Argument 2: What version of the docs we are building.
-version="${2:-${DOCS_VERSION:-master}}"
+version="${2:-${DOCS_VERSION:-main}}"
 if [ -z "$version" ]; then
 echo "error: python_doc_push_script.sh: version (arg2) not specified"
   exit 1
@@ -37,9 +37,9 @@ echo "error: python_doc_push_script.sh: install_path (arg1) not specified"
   exit 1
 fi
 
-is_master_doc=false
-if [ "$version" == "master" ]; then
-  is_master_doc=true
+is_main_doc=false
+if [ "$version" == "main" ]; then
+  is_main_doc=true
 fi
 
 # Argument 3: The branch to push to. Usually is "site"
@@ -86,7 +86,7 @@ pushd docs
 
 # Build the docs
 pip -q install -r requirements.txt
-if [ "$is_master_doc" = true ]; then
+if [ "$is_main_doc" = true ]; then
   build_docs html
   [ $? -eq 0 ] || exit $?
   make coverage

.circleci/cimodel/data/pytorch_build_definitions.py
@@ -185,7 +185,7 @@ def gen_docs_configs(xenial_parent_config):
         HiddenConf(
             "pytorch_python_doc_build",
             parent_build=xenial_parent_config,
-            filters=gen_filter_dict(branches_list=["master", "nightly"],
+            filters=gen_filter_dict(branches_list=["master", "main", "nightly"],
                                     tags_list=RC_PATTERN),
         )
     )
@@ -201,7 +201,7 @@ def gen_docs_configs(xenial_parent_config):
         HiddenConf(
             "pytorch_cpp_doc_build",
             parent_build=xenial_parent_config,
-            filters=gen_filter_dict(branches_list=["master", "nightly"],
+            filters=gen_filter_dict(branches_list=["master", "main", "nightly"],
                                     tags_list=RC_PATTERN),
         )
     )

.circleci/cimodel/data/simple/util/branch_filters.py
@@ -1,4 +1,5 @@
 NON_PR_BRANCH_LIST = [
+    "main",
     "master",
     r"/ci-all\/.*/",
     r"/release\/.*/",

.circleci/config.yml
@@ -1085,7 +1085,7 @@ jobs:
     parameters:
       branch:
         type: string
-        default: "master"
+        default: "main"
     steps:
     - attach_workspace:
         at: /tmp/workspace
@@ -1125,7 +1125,7 @@ jobs:
           echo "DOCKER_IMAGE: "${COMMIT_DOCKER_IMAGE}
           # turn v1.12.0rc3 into 1.12
           tag=$(echo $CIRCLE_TAG | sed -e 's/v*\([0-9]*\.[0-9]*\).*/\1/')
-          target=${tag:-master}
+          target=${tag:-main}
           echo "building for ${target}"
           time docker pull ${COMMIT_DOCKER_IMAGE} >/dev/null
           export id=$(docker run --env-file "${BASH_ENV}" --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -t -d -w /var/lib/jenkins ${COMMIT_DOCKER_IMAGE})
@@ -1135,7 +1135,7 @@ jobs:
           echo ${COMMAND} > ./command.sh && unbuffer bash ./command.sh | ts
 
           mkdir -p ~/workspace/build_artifacts
-          docker cp $id:/var/lib/jenkins/workspace/pytorch.github.io/docs/master ~/workspace/build_artifacts
+          docker cp $id:/var/lib/jenkins/workspace/pytorch.github.io/docs/main ~/workspace/build_artifacts
           docker cp $id:/var/lib/jenkins/workspace/pytorch.github.io /tmp/workspace
 
           # Save the docs build so we can debug any problems
@@ -1147,7 +1147,7 @@ jobs:
         paths:
           - .
     - store_artifacts:
-        path: ~/workspace/build_artifacts/master
+        path: ~/workspace/build_artifacts/main
         destination: docs
 
   pytorch_cpp_doc_build:
@@ -1171,12 +1171,12 @@ jobs:
           echo "DOCKER_IMAGE: "${COMMIT_DOCKER_IMAGE}
           # turn v1.12.0rc3 into 1.12
           tag=$(echo $CIRCLE_TAG | sed -e 's/v*\([0-9]*\.[0-9]*\).*/\1/')
-          target=${tag:-master}
+          target=${tag:-main}
           echo "building for ${target}"
           time docker pull ${COMMIT_DOCKER_IMAGE} >/dev/null
           export id=$(docker run --env-file "${BASH_ENV}" --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -t -d -w /var/lib/jenkins ${COMMIT_DOCKER_IMAGE})
 
-          export COMMAND='((echo "sudo chown -R jenkins workspace && cd workspace && '"export CIRCLE_SHA1='$CIRCLE_SHA1'"' && . ./.circleci/scripts/cpp_doc_push_script.sh docs/"$target" master") | docker exec -u jenkins -i "$id" bash) 2>&1'
+          export COMMAND='((echo "sudo chown -R jenkins workspace && cd workspace && '"export CIRCLE_SHA1='$CIRCLE_SHA1'"' && . ./.circleci/scripts/cpp_doc_push_script.sh docs/"$target" main") | docker exec -u jenkins -i "$id" bash) 2>&1'
 
           echo ${COMMAND} > ./command.sh && unbuffer bash ./command.sh | ts
 
@@ -1660,7 +1660,7 @@ jobs:
           time docker pull ${DOCKER_IMAGE}:${DOCKER_TAG} >/dev/null
           export id=$(docker run --env-file "${BASH_ENV}" --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -t -d -w /var/lib/jenkins ${DOCKER_IMAGE}:${DOCKER_TAG})
 
-          echo "Do NOT merge master branch into $CIRCLE_BRANCH in environment $BUILD_ENVIRONMENT"
+          echo "Do NOT merge main branch into $CIRCLE_BRANCH in environment $BUILD_ENVIRONMENT"
 
           git submodule sync && git submodule update -q --init --recursive --depth 1 --jobs 0
 
@@ -1918,6 +1918,7 @@ workflows:
           filters:
             branches:
               only:
+                - main
                 - master
                 - /ci-all\/.*/
                 - /release\/.*/
@@ -1930,6 +1931,7 @@ workflows:
           filters:
             branches:
               only:
+                - main
                 - master
                 - /ci-all\/.*/
                 - /release\/.*/
@@ -1942,6 +1944,7 @@ workflows:
           filters:
             branches:
               only:
+                - main
                 - master
                 - /ci-all\/.*/
                 - /release\/.*/
@@ -1961,6 +1964,7 @@ workflows:
           filters:
             branches:
               only:
+                - main
                 - master
                 - /ci-all\/.*/
                 - /release\/.*/

.circleci/generate_config_yml.py
@@ -81,11 +81,11 @@ def _for_all_items(items, functor) -> None:
         functor(item_type, item)
 
 def filter_master_only_jobs(items):
-    def _is_master_item(item):
+    def _is_main_or_master_item(item):
         filters = item.get('filters', None)
         branches = filters.get('branches', None) if filters is not None else None
         branches_only = branches.get('only', None) if branches is not None else None
-        return 'master' in branches_only if branches_only is not None else False
+        return ('main' in branches_only or 'master' in branches_only) if branches_only is not None else False
 
     master_deps = set()
 
@@ -94,7 +94,7 @@ def filter_master_only_jobs(items):
         item_name = item.get("name", None)
         if not isinstance(requires, list):
             return
-        if _is_master_item(item) or item_name in master_deps:
+        if _is_main_or_master_item(item) or item_name in master_deps:
             master_deps.update([n.strip('"') for n in requires])
 
     def _do_filtering(items):
@@ -105,7 +105,7 @@ def filter_master_only_jobs(items):
         item_type, item = next(iter(items.items()))
         item_name = item.get("name", None)
         item_name = item_name.strip('"') if item_name is not None else None
-        if not _is_master_item(item) and item_name not in master_deps:
+        if not _is_main_or_master_item(item) and item_name not in master_deps:
             return None
         if 'filters' in item:
             item = item.copy()
@@ -113,7 +113,7 @@ def filter_master_only_jobs(items):
         return {item_type: item}
 
     # Scan of dependencies twice to pick up nested required jobs
-    # I.e. jobs depending on jobs that master-only job depend on
+    # I.e. jobs depending on jobs that main-only job depend on
     _for_all_items(items, _save_requires_if_master)
     _for_all_items(items, _save_requires_if_master)
     return _do_filtering(items)

.circleci/scripts/binary_checkout.sh
@@ -49,8 +49,9 @@ if [[ -n "${CIRCLE_PR_NUMBER:-}" ]]; then
   git reset --hard "$CIRCLE_SHA1"
 elif [[ -n "${CIRCLE_SHA1:-}" ]]; then
   # Scheduled workflows & "smoke" binary build on master on PR merges
+  DEFAULT_BRANCH="$(git remote show $CIRCLE_REPOSITORY_URL | awk '/HEAD branch/ {print $NF}')"
   git reset --hard "$CIRCLE_SHA1"
-  git checkout -q -B master
+  git checkout -q -B $DEFAULT_BRANCH
 else
   echo "Can't tell what to checkout"
   exit 1

.circleci/scripts/binary_linux_build.sh
@@ -26,7 +26,7 @@ else
   build_script='manywheel/build.sh'
 fi
 
-if [[ "$CIRCLE_BRANCH" == "master" ]] || [[ "$CIRCLE_BRANCH" == release/* ]]; then
+if [[ "$CIRCLE_BRANCH" == "main" ]] || [[ "$CIRCLE_BRANCH" == "master" ]] || [[ "$CIRCLE_BRANCH" == release/* ]]; then
   export BUILD_DEBUG_INFO=1
 fi
 

commit 2663b188f6fdf9a01016379883e92c8ce24d11a4

Fix doc build
Regression introduced in https://github.com/pytorch/pytorch/pull/73224
The caller for this script has never been updated to pass in main: https://github.com/pytorch/pytorch/blob/2ecc59086a5bc896c82f6f309d25569ec2933467/.github/workflows/_docs.yml#L81-L85

So this change made it so that all PR doc is built as-if it was a release (for example https://github.com/pytorch/pytorch/runs/6031182009?check_suite_focus=true) and so the coverage test for the doc didn't run for a month :(
Pull Request resolved: https://github.com/pytorch/pytorch/pull/75997
Approved by: https://github.com/musebc, https://github.com/seemethere

.circleci/scripts/cpp_doc_push_script.sh
@@ -20,7 +20,7 @@ echo "cpp_doc_push_script.sh: Invoked with $*"
 #       but since DOCS_INSTALL_PATH can be derived from DOCS_VERSION it's probably better to
 #       try and gather it first, just so we don't potentially break people who rely on this script
 # Argument 2: What version of the Python API docs we are building.
-version="${2:-${DOCS_VERSION:-main}}"
+version="${2:-${DOCS_VERSION:-master}}"
 if [ -z "$version" ]; then
 echo "error: cpp_doc_push_script.sh: version (arg2) not specified"
   exit 1
@@ -35,7 +35,7 @@ echo "error: cpp_doc_push_script.sh: install_path (arg1) not specified"
 fi
 
 is_main_doc=false
-if [ "$version" == "main" ]; then
+if [ "$version" == "master" ]; then
   is_main_doc=true
 fi
 

.circleci/scripts/python_doc_push_script.sh
@@ -23,7 +23,7 @@ set -ex
 #       but since DOCS_INSTALL_PATH can be derived from DOCS_VERSION it's probably better to
 #       try and gather it first, just so we don't potentially break people who rely on this script
 # Argument 2: What version of the docs we are building.
-version="${2:-${DOCS_VERSION:-main}}"
+version="${2:-${DOCS_VERSION:-master}}"
 if [ -z "$version" ]; then
 echo "error: python_doc_push_script.sh: version (arg2) not specified"
   exit 1
@@ -38,7 +38,7 @@ echo "error: python_doc_push_script.sh: install_path (arg1) not specified"
 fi
 
 is_main_doc=false
-if [ "$version" == "main" ]; then
+if [ "$version" == "master" ]; then
   is_main_doc=true
 fi
 

docs/source/conf.py
@@ -166,6 +166,78 @@ coverage_ignore_classes = [
     "finfo",
     "iinfo",
     "qscheme",
+    "AggregationType",
+    "AliasDb",
+    "AnyType",
+    "Argument",
+    "ArgumentSpec",
+    "BenchmarkConfig",
+    "BenchmarkExecutionStats",
+    "Block",
+    "BoolType",
+    "BufferDict",
+    "CallStack",
+    "Capsule",
+    "ClassType",
+    "Code",
+    "CompleteArgumentSpec",
+    "ComplexType",
+    "ConcreteModuleType",
+    "ConcreteModuleTypeBuilder",
+    "DeepCopyMemoTable",
+    "DeserializationStorageContext",
+    "DeviceObjType",
+    "DictType",
+    "EnumType",
+    "ExecutionPlan",
+    "FileCheck",
+    "FloatType",
+    "FunctionSchema",
+    "Gradient",
+    "Graph",
+    "GraphExecutorState",
+    "IODescriptor",
+    "InferredType",
+    "IntType",
+    "InterfaceType",
+    "ListType",
+    "LockingLogger",
+    "MobileOptimizerType",
+    "ModuleDict",
+    "Node",
+    "NoneType",
+    "NoopLogger",
+    "NumberType",
+    "OperatorInfo",
+    "OptionalType",
+    "ParameterDict",
+    "PyObjectType",
+    "PyTorchFileReader",
+    "PyTorchFileWriter",
+    "RRefType",
+    "ScriptClass",
+    "ScriptClassFunction",
+    "ScriptDict",
+    "ScriptDictIterator",
+    "ScriptDictKeyIterator",
+    "ScriptList",
+    "ScriptListIterator",
+    "ScriptMethod",
+    "ScriptModule",
+    "ScriptModuleSerializer",
+    "ScriptObject",
+    "ScriptObjectProperty",
+    "SerializationStorageContext",
+    "StaticModule",
+    "StringType",
+    "SymIntType",
+    "ThroughputBenchmark",
+    "TracingState",
+    "TupleType",
+    "Type",
+    "UnionType",
+    "Use",
+    "Value",
     # torch.cuda
     "BFloat16Storage",
     "BFloat16Tensor",